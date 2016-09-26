99d09140a4b959d66120d06f492fc06fa542e1d5
Alex Sexton <alex@dxw.com>
2016-09-26 15:06:42 +0100

    gruntfile uses standard.js syntax

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,42 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-imagemin')
+  grunt.loadNpmTasks('grunt-contrib-watch')
 
-  grunt.loadNpmTasks("grunt-contrib-sass");
-  grunt.loadNpmTasks("grunt-contrib-concat");
-  grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks("grunt-contrib-imagemin");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-
-
-  grunt.registerTask("default", ["watch", "uglify", "concat", "imagemin"]);
+  grunt.registerTask('default', ['watch', 'uglify', 'concat', 'imagemin'])
 
   grunt.initConfig({
 
-    pkg: grunt.file.readJSON("package.json"),
+    pkg: grunt.file.readJSON('package.json'),
 
     sass: {
       dist: {
         options: {
-          style: "expanded",
+          style: 'expanded'
         },
         files: {
-          "static/css/screen.css" : "assets/scss/screen.scss"
+          'static/css/screen.css': 'assets/scss/screen.scss'
         }
       }
     },
 
     concat: {
       dist: {
-        src: ["assets/js/plugins/*.js"],
-        dest: "assets/js/plugins.js",
-      },
+        src: ['assets/js/plugins/*.js'],
+        dest: 'assets/js/plugins.js'
+      }
     },
 
     uglify: {
       options: {
-        seperator: ";\n",
+        seperator: ';\n'
       },
       js: {
         files: {
-        "static/js/main.min.js" : "assets/js/main.js",
-        "static/js/plugins.min.js" : "assets/js/plugins.js"
+          'static/js/main.min.js': 'assets/js/main.js',
+          'static/js/plugins.min.js': 'assets/js/plugins.js'
         }
       }
     },
@@ -46,25 +44,24 @@ module.exports = function(grunt) {
     imagemin: {
       dist: {
         files: [{
-            expand: true,
-            cwd: "assets/images",
-            src: ["**/*.{png,jpg,gif}"],
-            dest: "static/images",
-        }],
-      },
+          expand: true,
+          cwd: 'assets/images',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'static/images'
+        }]
+      }
     },
 
     watch: {
       css: {
-        files: ["**/*.sass", "**/*.scss"],
-        tasks: ["sass"]
+        files: ['**/*.sass', '**/*.scss'],
+        tasks: ['sass']
       },
       js: {
-        files: ["static/**/*.js", "assets/**/*.js"],
-        tasks: ["uglify", "concat"]
+        files: ['static/**/*.js', 'assets/**/*.js'],
+        tasks: ['uglify', 'concat']
       }
-    },
+    }
 
-  });
-
+  })
 }
