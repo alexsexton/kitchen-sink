622259601a491d3bc22835f415e2986bce6681db
Alex Sexton <alex@dxw.com>
2016-09-29 10:58:19 +0100

    Silly: fixed ref to sass task runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-sass')
   grunt.loadNpmTasks('grunt-contrib-concat')
   grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-contrib-imagemin')
