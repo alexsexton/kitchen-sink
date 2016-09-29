7d9ae7458a990e9996adcec5949c3c78956a116c
Alex Sexton <alex@dxw.com>
2016-09-29 11:17:27 +0100

    Refactor: Removed a load of unused JS and added standard comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,53 @@
+/* globals jQuery, */
 // Please, please comment your JS. Not everyone knows it as well as you.
 
-// Tabs
-$(document).ready(function(){
-  var tabs = $(".tab");
-  tabs.hide().filter(":first").attr("aria-hidden","true").show();
+'use strict'
 
-  $(".tabs ul a").click(function(){
-    tabs.hide().attr("aria-hidden","true");
-    tabs.filter(this.hash).attr("aria-hidden","false").show();
-    $(".tabs ul a").removeClass("selected").attr("aria-expanded","false");
-    $(this).addClass("selected").attr("aria-expanded","true");
-    return false;
-  }).filter(":first").click().attr("aria-expanded","true");
-});
+// Depenedencies
+// var enquire = require('../../*.js')
+require('../../bower_components/jquery-accessibleMegaMenu/js/jquery-accessibleMegaMenu.js')
+require('../../bower_components/fitvids/jquery.fitvids.js')
 
-// Add selected class and aria roles to checked input labels
-$(document).ready(function(){
-  $("input[type=radio],input[type=checkbox]").change(function(){
-    $("input[type=radio],input[type=checkbox]").attr("aria-checked","false").parent().removeClass("selected");
-    $("input[type=radio]:checked,input[type=checkbox]:checked").attr("aria-checked","true").parent().addClass("selected");
-  });
-});
+jQuery(function ($) {
+  // Tabs
+  $(document).ready(function () {
+    var tabs = $('.tab')
+    tabs.hide().filter(':first').attr('aria-hidden', 'true').show()
 
-// FitVids
-$(document).ready(function(){
-  $(".video-frame").fitVids();
-});
+    $('.tabs ul a').click(function () {
+      tabs.hide().attr('aria-hidden', 'true')
+      tabs.filter(this.hash).attr('aria-hidden', 'false').show()
+      $('.tabs ul a').removeClass('selected').attr('aria-expanded', 'false')
+      $(this).addClass('selected').attr('aria-expanded', 'true')
+      return false
+    }).filter(':first').click().attr('aria-expanded', 'true')
+  })
 
-// Navigation
-$(document).ready(function(){
-  $("#js-main-navigation").accessibleMegaMenu({
-    uuidPrefix: "accessible-nav",
-    menuClass: "nav-menu",
-    topNavItemClass: "nav-item",
-    panelClass: "sub-nav",
-    panelGroupClass: "sub-nav-group",
-    hoverClass: "hover",
-    focusClass: "focus",
-    openClass: "open"
-  });
-});
+  // Add selected class and aria roles to checked input labels
+  $(document).ready(function () {
+    $('input[type=radio],input[type=checkbox]').change(function () {
+      $('input[type=radio],input[type=checkbox]').attr('aria-checked', 'false').parent().removeClass('selected')
+      $('input[type=radio]:checked,input[type=checkbox]:checked').attr('aria-checked', 'true').parent().addClass('selected')
+    })
+  })
 
-// BX Slider
-$(document).ready(function(){
-  $(".slider").bxSlider();
-});
+  // FitVids
+  $(document).ready(function () {
+    $('.video-frame').fitVids()
+  })
+
+  // Navigation
+  $(document).ready(function () {
+    $('#js-main-navigation').accessibleMegaMenu({
+      uuidPrefix: 'accessible-nav',
+      menuClass: 'nav-menu',
+      topNavItemClass: 'nav-item',
+      panelClass: 'sub-nav',
+      panelGroupClass: 'sub-nav-group',
+      hoverClass: 'hover',
+      focusClass: 'focus',
+      openClass: 'open'
+    })
+  })
+// End
+})
