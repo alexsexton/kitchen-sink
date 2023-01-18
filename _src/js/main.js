f64ed9f147d6aed5eac81cf1affb29618e313fe0
Alex Sexton <alex@alexsexton.cc>
2023-01-18 15:50:18 +0000

    Del: unused old things

--- _src/js/main.js
+++
@@ -1,8 +0,0 @@
-/* globals */
-'use strict'
-
-// Add loaded class once everything has loaded
-document.addEventListener('DOMContentLoaded', (event) => {
-  const doc = document.querySelector('html')
-  doc.classList.add('loaded')
-})
