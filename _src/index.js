5c62b213e116ea8f5fa2774c0099ea9367048b2a
Alex Sexton <alex@alexsexton.cc>
2023-01-18 18:04:37 +0000

    Set up Initial webpack config and build

--- _src/index.js
+++ _src/index.js
@@ -1,8 +1,11 @@
-/* globals */
-'use strict'
-
-// Add loaded class once everything has loaded
-document.addEventListener('DOMContentLoaded', (event) => {
-  const doc = document.querySelector('html')
-  doc.classList.add('loaded')
-})
+import './scss/styles.scss'
+import './js/scroll-magic.js'
+//import './js/nav-toggle.js'
+;(function () {
+  // Add loaded class once everything has loaded
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const doc = document.querySelector('html')
+    doc.classList.add('loaded')
+  })
+  // eol
+})()
--- _src/js/index.js
+++
@@ -1,9 +0,0 @@
-import './scss/style.scss'
-import './js/scroll-magic.js'
-import './js/nav-toggle.js'
-
-// Add loaded class once everything has loaded
-document.addEventListener('DOMContentLoaded', (event) => {
-  const doc = document.querySelector('html')
-  doc.classList.add('loaded')
-})
